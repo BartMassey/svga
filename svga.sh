6d4a875813a7a3b098ffded660d6c93e30e1c44b
Bart Massey <bart@cs.pdx.edu>
2015-07-06 16:33:25 -0700

    got automatic mode selection working

--- svga.sh
+++ svga.sh
@@ -8,56 +8,93 @@
 TMP="/tmp/svga.$$"
 trap "rm -f $TMP" 0 1 2 3 15
 
-MODE=best
-case $# in
-1)
-        MODE=$1
-        ;;
-esac
+xrandr |
+egrep -v '^Screen' |
+awk '
+  $0~/^ / {
+    if (current)
+       modes[current][++nmc] = $1
+    next
+  }
+  $2=="connected" {
+    if ($3 == "primary")
+       primary = $1
+    else
+       alt[++nalt] = $1
+    current = $1
+  }
+  END {
+    if (primary) {
+       for (i in modes[primary]) {
+	   found_matches = 1
+           for (j in alt) {
+	       found_match = 0
+               for (k in modes[alt[j]]) {
+                   if (modes[alt[j]][k] == modes[primary][i]) {
+		       found_match = 1
+		       break
+		   }
+	       }
+	       if (!found_match) {
+		   found_matches = 0
+		   break
+	       }
+           }
+	   if (found_matches)
+	       ok_modes[++nok_modes] = modes[primary][i]
+       }
+       if (nok_modes) {
+	   best_pixels = 0
+	   for (i in ok_modes) {
+	       ndims = split(ok_modes[i], dims, "x")
+	       if (ndims != 2)
+		   continue
+	       pixels = dims[1] * dims[2]
+	       if (pixels == 0)
+		   continue
+	       if (pixels > best_pixels) {
+		   best_pixels = pixels
+		   best_mode = ok_modes[i]
+	       }
+	   }
+	   if (best_mode) {
+	       print best_mode
+               print primary
+	       for (i in alt)
+		   print alt[i]
+	   }
+       }
+    }
+  }' >$TMP
 
-xrandr >$TMP
-OUTPUTS="`egrep -v '^ ' $TMP | egrep -v '^Screen' | awk '
-$2==\"connected\" {
-  if ($3 == \"primary\")
-     primary=$1
-  else
-     alt[++n]=$1
-}
-END {
-  if (primary)
-     print primary
-  for (i in alt)
-     print alt[i]
-}'`"
-
-for OUTPUT in $OUTPUTS
-do
-    echo "saw output: $OUTPUT"
-done
-exit 0
-
-if [ "$1" = off ]
+if [ "`cat $TMP`" = "" ]
 then
-    EXTRA=false
-    xrandr -s 0
-    for OUTPUT in $OUTPUTS
-    do
-	$EXTRA && xrandr --output "$OUTPUT" --off
-	EXTRA=true
-    done
-    exit 0
-fi
-
-exit 0
-
-if [ OUTPUTS = '' ]
-then
-    echo "no extra outputs to enable" >&2
+    echo "not enough information, giving up" >&2
     exit 1
 fi
 
-xrandr -s "$MODE"
-for OUTPUT in $OUTPUTS
-do
-    xrandr --output "$OUTPUT" --off --mode "$MODE"
-done
+cat $TMP | (
+  read MODE
+  read PRIMARY
+
+  if [ $# -eq 1 ]
+  then
+      MODE="$1"
+  fi
+
+  if [ "$MODE" = off ]
+  then
+      xrandr -s 0
+      while read OUTPUT
+      do
+	  xrandr --output "$OUTPUT" --off
+      done
+      exit 0
+  fi
+
+  xrandr --output "$PRIMARY" --mode $MODE
+  while read OUTPUT
+  do
+      xrandr --output "$OUTPUT" --off --mode "$MODE" --same-as "$PRIMARY"
+  done
+)
